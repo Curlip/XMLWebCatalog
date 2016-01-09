18b67c407f1f1d8d18cbd215afade0e9392e24f3
Curlip <curlipcool@gmail.com>
2016-01-10 10:26:52 +1100

    Inproved the reload script.
    Now takes one param: a path to Saxon.jar.

--- reload.sh
+++ reload.sh
@@ -1,3 +1,10 @@
 #!/bin/bash    
 
 java -jar $1 catalog.xml splitter.xsl
+java -jar $1 catalog.xml style.xsl -o:index.html
+
+find d -name "*.xml" -exec bash -c 'java -jar $2 $1 d/style.xsl -o:"${1%.xml}.html"' _ {} $1 \;
+
+find . -type f -exec chmod 644 {} \; && find . -type d -exec chmod 755 {} \;
+
+chmod a+x reload.sh
